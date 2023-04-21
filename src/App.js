7e7af09a983a7695a0d7d0faef4d74b744e15363
manymorehyun <123722508+moreHyun@users.noreply.github.com>
2023-04-21 16:31:33 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+/* BrowserRouter 컴포넌트를 이용한 라우팅 설정 */
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './pages/Main';
+import About from './pages/About';
+import Menu from './pages/Menu';
 
 function App() {
   return (
     <>
-      테스트
+      <BrowserRouter>
+        <Routes>
+          {/* 루트 경로로 요청 시 Main 컴포넌트를 보여준다. */}
+          {/* <Route path="/" element={ <Main/> }/> */}
+
+          {/* 인덱스로 설정해두면 루트 요청과 동일하다. */}
+          <Route index element={<Main />} />
+
+          <Route path="/about" element={ <About/> }/>
+          <Route path="/menu" element={ <Menu/> }/>
+          
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
